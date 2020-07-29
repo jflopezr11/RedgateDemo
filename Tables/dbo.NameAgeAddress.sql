dcf31e46282d7699e68e7a16bfb2d9c5c8366b8b
Joshua Lopez-Roque <joshualopezroque@gmail.com>
2020-07-29 14:52:09 -0700

    Made new table

---
+++ Tables/dbo.NameAgeAddress.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[NameAgeAddress]
+(
+[Name] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Age] [int] NULL,
+[Address] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+) ON [PRIMARY]
+GO
